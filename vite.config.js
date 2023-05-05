9b1dd47cd1dacd36656118296b6a5caaf4b81183
Ahmed Gad <eng.ahmedmgad@gmail.com>
2023-05-05 22:51:43 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     build: {
-        outDir: app_path + '/public/build-forms',
+        outDir: __dirname + '/dist/build-forms',
         emptyOutDir: true,
         manifest: true,
     },
