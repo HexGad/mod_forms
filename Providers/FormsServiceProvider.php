57b02baac08554abb1dc323967bade3fb15f896a
Ahmed Gad <eng.ahmedmgad@gmail.com>
2023-05-06 00:16:11 +0300

    update

--- Providers/FormsServiceProvider.php
+++ Providers/FormsServiceProvider.php
@@ -50,7 +50,7 @@ class FormsServiceProvider extends ServiceProvider
     {
         $this->publishes([
             module_path($this->moduleName, 'dist/build-forms') => public_path(),
-        ], 'assets');
+        ], 'modules-assets');
     }
 
 
